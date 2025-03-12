28677e6d3f080732d43c4b505c432a869a639a79
Alexander Shulgin <shulgin.alexandera@gmail.com>
2025-03-12 14:55:47 +0300

    исправлена ошибка погоды

--- src/components/weather-block/weather-block.jsx
+++ src/components/weather-block/weather-block.jsx
@@ -1,60 +1,132 @@
+// import { useEffect, useState } from 'react';
+// import styles from './weather-block.module.css';
+// import { createWetherIcon } from './utils/icon-weather';
+
+// export const WeatherBlock = () => {
+// 	const [city, setCity] = useState('');
+// 	const [temperature, setTemperature] = useState('');
+// 	const [weather, setWeather] = useState('');
+// 	const [weatherIcon, setWeatherIcon] = useState('');
+
+// 	useEffect(() => {
+// 		fetch('https://api.sypexgeo.net/json/')
+// 			.then((response) => response.json())
+// 			.then((data) => {
+// 				const cityName = data.city.name_en;
+
+// 				return cityName;
+// 			})
+// 			.then((cityName) => {
+// 				fetch(
+// 					`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ru&appid=acd4f346c669d7400f4dbbeb7f1350e0`,
+// 				)
+// 					.then((res) => res.json())
+// 					.then(({ main, name, weather }) => {
+// 						setCity(name);
+// 						setTemperature(Math.round(main.temp));
+// 						setWeather(weather[0].description);
+// 						setWeatherIcon(createWetherIcon(weather[0].description));
+// 					});
+// 			})
+// 			.catch((error) => console.log(error));
+// 	}, []);
+
+// 	return (
+// 		<div className={styles.footer_weather}>
+// 			<div>
+// 				{city},{' '}
+// 				<div>
+// 					{new Date().toLocaleDateString('ru', {
+// 						day: 'numeric',
+// 						month: 'long',
+// 					})}
+// 				</div>
+// 			</div>
+// 			<div className={styles.footer_weather_text}>
+// 				<div className={styles.footer_weather_block}>
+// 					<div>{weather}</div>
+// 					<div>
+// 						{temperature}°C
+// 						<img
+// 							className={styles.footer_weather_icon}
+// 							src={weatherIcon}
+// 							alt="weather-icon"
+// 						/>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+
 import { useEffect, useState } from 'react';
 import styles from './weather-block.module.css';
 import { createWetherIcon } from './utils/icon-weather';
 
 export const WeatherBlock = () => {
-	const [city, setCity] = useState('');
-	const [temperature, setTemperature] = useState('');
-	const [weather, setWeather] = useState('');
-	const [weatherIcon, setWeatherIcon] = useState('');
+  const [city, setCity] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [weather, setWeather] = useState('');
+  const [weatherIcon, setWeatherIcon] = useState('');
 
-	useEffect(() => {
-		fetch('https://api.sypexgeo.net/json/')
-			.then((response) => response.json())
-			.then((data) => {
-				const cityName = data.city.name_en;
+  useEffect(() => {
+    const fetchWeatherData = async (lat, lon) => {
+      const API_KEY = 'acd4f346c669d7400f4dbbeb7f1350e0'; // Ваш ключ API
+      const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=ru&appid=${API_KEY}`);
+      const { main, name, weather } = await weatherResponse.json();
+      setCity(name);
+      setTemperature(Math.round(main.temp));
+      setWeather(weather[0].description);
+      setWeatherIcon(createWetherIcon(weather[0].description));
+    };
 
-				return cityName;
-			})
-			.then((cityName) => {
-				fetch(
-					`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ru&appid=acd4f346c669d7400f4dbbeb7f1350e0`,
-				)
-					.then((res) => res.json())
-					.then(({ main, name, weather }) => {
-						setCity(name);
-						setTemperature(Math.round(main.temp));
-						setWeather(weather[0].description);
-						setWeatherIcon(createWetherIcon(weather[0].description));
-					});
-			})
-			.catch((error) => console.log(error));
-	}, []);
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const { latitude, longitude } = position.coords;
+            fetchWeatherData(latitude, longitude);
+          },
+          () => {
+            console.error('Ошибка получения местоположения');
+			
+			fetchWeatherData(56.5011, 84.9895);
+          }
+        );
+      } else {
+        console.error('Geolocation не поддерживается вашим браузером');
+		fetchWeatherData(56.5011, 84.9895);
+      }
+    };
 
-	return (
-		<div className={styles.footer_weather}>
-			<div>
-				{city},{' '}
-				<div>
-					{new Date().toLocaleDateString('ru', {
-						day: 'numeric',
-						month: 'long',
-					})}
-				</div>
-			</div>
-			<div className={styles.footer_weather_text}>
-				<div className={styles.footer_weather_block}>
-					<div>{weather}</div>
-					<div>
-						{temperature}°C
-						<img
-							className={styles.footer_weather_icon}
-							src={weatherIcon}
-							alt="weather-icon"
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    getLocation();
+  }, []);
+
+  return (
+    <div className={styles.footer_weather}>
+      <div>
+        {city},{' '}
+        <div>
+          {new Date().toLocaleDateString('ru', {
+            day: 'numeric',
+            month: 'long',
+          })}
+        </div>
+      </div>
+      <div className={styles.footer_weather_text}>
+        <div className={styles.footer_weather_block}>
+          <div>{weather}</div>
+          <div>
+            {temperature}°C
+            <img
+              className={styles.footer_weather_icon}
+              src={weatherIcon}
+              alt="weather-icon"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
