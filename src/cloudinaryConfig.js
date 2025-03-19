dea55ddcf445bbc8e9a8878596bf001192bd3691
Alexander Shulgin <shulgin.alexandera@gmail.com>
2025-03-19 10:31:54 +0300

    добавлен env

--- src/cloudinaryConfig.js
+++ src/cloudinaryConfig.js
@@ -1,10 +1,11 @@
 // import { upload } from "@testing-library/user-event/dist/upload"; 
 
 const cloudinaryConfig = {
-    cloudName: 'dgdfwffbt',
-    apiKey: '151325144872447',
-    apiSecret: 'Odi-A6GaYGePocY237sRxFM57kI',
-    uploadPreset: 'boat_fix'
-  };
+  cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.REACT_APP_CLOUDINARY_API_KEY,
+  apiSecret: process.env.REACT_APP_CLOUDINARY_API_SECRET,
+  uploadPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
+};
+
   
   export default cloudinaryConfig;
--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8FFo9yJwBBOwXSS947UvtBPy5I7GevXo",
-  authDomain: "boatfix-97d3f.firebaseapp.com",
-  projectId: "boatfix-97d3f",
-  storageBucket: "boatfix-97d3f.firebasestorage.app",
-  messagingSenderId: "369442658333",
-  appId: "1:369442658333:web:a691a7d09fd5acfebf4ef4",
-  measurementId: "G-TSTYV5L4JK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
