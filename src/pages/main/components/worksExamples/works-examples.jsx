bd33392eb6b44ebc356b78f571e677bfd8d18bc7
Alexander Shulgin <shulgin.alexandera@gmail.com>
2025-03-28 14:33:23 +0300

    добавлено модальное окно для зума изображений на главной странице

--- src/pages/main/components/worksExamples/works-examples.jsx
+++ src/pages/main/components/worksExamples/works-examples.jsx
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 import example1Before from "../../../../image/examples/example1Before.jpg";
 import example1After from "../../../../image/examples/example1After.jpg";
 import example2Before from "../../../../image/examples/example2Before.jpg";
 import example2After from "../../../../image/examples/example2After.jpg";
 import styles from "./works-examples.module.css";
 export const WorksExamples = () => {
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState("");
+
+  const openModal = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedImage("");
+  };
   return (
     <div className={styles.works_repair_info_container}>
       <div className={styles.works_example_item}>
@@ -18,12 +31,12 @@ export const WorksExamples = () => {
         </div>
         <div className={styles.works_repair_example}>
           <h3 className={styles.works_repair_example_title}>До ремонта</h3>
-          <img src={example1Before} alt="Before repair boat" loading="lazy" />
+          <img src={example1Before} alt="Before repair boat" loading="lazy" onClick={() => openModal(example1Before)} />
           <p>Повреждение уключины, изношена ось уключины</p>
         </div>
         <div className={styles.works_repair_example}>
           <h3 className={styles.works_repair_example_title}>После ремонта</h3>
-          <img src={example1After} alt="After repair boat" loading="lazy" />
+          <img src={example1After} alt="After repair boat" loading="lazy" onClick={() => openModal(example1After)}/>
           <p>Замена уключины, после ремонта</p>
         </div>
       </div>
@@ -38,15 +51,28 @@ export const WorksExamples = () => {
         </div>
         <div className={styles.works_repair_example}>
           <h3 className={styles.works_repair_example_title}>До ремонта</h3>
-          <img src={example2Before} alt="Before repair boat" loading="lazy" />
+          <img src={example2Before} alt="Before repair boat" loading="lazy" onClick={() => openModal(example2Before)}/>
           <p>Выбор места и подготовка поверхности</p>
         </div>
         <div className={styles.works_repair_example}>
           <h3 className={styles.works_repair_example_title}>После ремонта</h3>
-          <img src={example2After} alt="After repair boat" loading="lazy" />
+          <img src={example2After} alt="After repair boat" loading="lazy" onClick={() => openModal(example2After)} />
           <p>Произведён монтаж и вклейка ручки для переноса лодки</p>
         </div>
       </div>
+
+      {isModalOpen && (
+              <div className={styles.modal} onClick={closeModal}>
+                <img
+                  src={selectedImage}
+                  alt="Large View"
+                  className={styles.modal_image}
+                />
+                <button className={styles.close_button} onClick={closeModal}>
+                  &times;
+                </button>
+              </div>
+            )}
     </div>
   );
 };
--- src/store.js
+++ src/store.js
@@ -1,12 +1,8 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { thunk } from "redux-thunk";
-import { appReducer, userReducer, orderReducer } from "./reducers";
 import { authReducer } from "./reducers/authReducer";
 
-const reducer = combineReducers({
-  app: appReducer,
-  user: userReducer,
-  order: orderReducer,
+const reducer = combineReducers({ 
   auth: authReducer,
 });
 
