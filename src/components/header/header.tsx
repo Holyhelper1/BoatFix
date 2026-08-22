8c076a74f49e968737a3cc917917b38013b3d661
Alexander Shulgin <shulgin.alexandera@gmail.com>
2026-08-22 19:19:39 +0300

    Переработана страница контактов

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,8 @@ import { CONTACTS, SOCIAL_LINKS } from "../../Constants/contacts";
 
 const NAV_ITEMS = [
   { label: "Главная", to: LINKS.MAIN },
-  { label: "До / После", to: `${LINKS.MAIN}#examples` },
+  // { label: "До / После", to: `${LINKS.MAIN}#examples` },
+  { label: "Оставить заявку", to: LINKS.ORDER },
   { label: "Цены", to: LINKS.PRICES },
   { label: "Контакты", to: LINKS.CONTACTS },
 ];
